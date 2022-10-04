1f7a00cb2b5d0a477efc146b656389807cb3d45f
scare-destiny <zhenya.venger@gmail.com>
2022-10-04 14:02:12 +0300

    removed clerk

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn
-} from '@clerk/nextjs'
 import { useRouter } from 'next/router'
 import { Nav } from '../components/Layout/Nav'
 
 const publicPages = ['/', `/recipes/[id]`]
 
 function MyApp({ Component, pageProps }) {
-  const { pathname } = useRouter()
-  const isPublicPage = publicPages.includes(pathname)
+	const { pathname } = useRouter()
+	const isPublicPage = publicPages.includes(pathname)
 
-  return (
-    <ClerkProvider>
-      <ChakraProvider>
-        {isPublicPage ? (
-          <>
-            <Nav />
-            <Component {...pageProps} />
-          </>
-        ) : (
-          <>
-            <SignedIn>
-              <Nav />
-              <Component {...pageProps} />
-            </SignedIn>
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
-          </>
-        )}
-      </ChakraProvider>
-    </ClerkProvider>
-  )
+	return (
+		<ChakraProvider>
+			{isPublicPage ? (
+				<>
+					<Nav />
+					<Component {...pageProps} />
+				</>
+			) : (
+				<>
+					<Nav />
+					<Component {...pageProps} />
+				</>
+			)}
+		</ChakraProvider>
+	)
 }
 
 export default MyApp
