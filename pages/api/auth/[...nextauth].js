1431e5b3b542dcb5b128d96638b583b43f7db908
scare-destiny <zhenya.venger@gmail.com>
2022-10-04 17:09:32 +0300

    yes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import NextAuth from 'next-auth'
 import FacebookProvider from 'next-auth/providers/facebook'
 
 export const authOptions = {
+	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
 		FacebookProvider({
 			clientId: process.env.FACEBOOK_CLIENT_ID,
 			clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
 		}),
 	],
-	secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions)
