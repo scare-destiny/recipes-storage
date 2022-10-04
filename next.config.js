0c137d1885d37c912215c27d39de28ca9b6e738f
scare-destiny <zhenya.venger@gmail.com>
2022-10-04 19:23:50 +0300

    roll bacvk

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
 	reactStrictMode: true,
 	trailingSlash: true,
 	env: {
-		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
-		FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+		NEXTAUTH_SECRET: 'Rs5F9TVf87O/NigI1/5Kv9wydgHoI3JMULIsk7V/i1E=',
+		FACEBOOK_CLIENT_ID: '1142200463385977',
+		FACEBOOK_CLIENT_SECRET: 'ee0cf361036cdda8a3d764ffc1305cc1',
 	},
 }
