f9d65ab81bcf6fc023689285705fae43886e8b8a
scare-destiny <zhenya.venger@gmail.com>
2022-10-05 22:12:09 +0300

    added new lib api and search image input for addrecipe

--- pages/addrecipe.js
+++ pages/addrecipe.js
@@ -6,10 +6,17 @@ import {
 	Heading,
 	Textarea,
 	Grid,
+	Box,
+	Container,
 	GridItem,
 	Button,
 	Select,
+	WrapItem,
+	IconButton,
+	InputGroup,
+	InputRightElement,
 } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 import { AutoResizeTextarea } from '../components/Layout/AutoResizeTextarea'
 import Link from 'next/link'
 import { useState } from 'react'
@@ -17,8 +24,18 @@ import { getAuth, signInWithCustomToken } from 'firebase/auth'
 import { collection, getDocs, addDoc } from 'firebase/firestore/lite'
 import { database } from '../firebase'
 import categories from '../data/categories'
+import { getCuratedPhotos, getQueryPhotos } from '../lib/api'
 
-export default function AddRecipe() {
+export async function getServerSideProps() {
+	const data = await getCuratedPhotos()
+	return {
+		props: {
+			data,
+		},
+	}
+}
+
+export default function AddRecipe({ data }) {
 	const [recipe, setRecipe] = useState({
 		title: '',
 		description: '',
@@ -31,6 +48,10 @@ export default function AddRecipe() {
 		servings: '',
 		calories: '',
 	})
+
+	const [photos, setPhotos] = useState(data)
+	const [query, setQuery] = useState('')
+
 	const handleChange = (e) => {
 		setRecipe({
 			...recipe,
@@ -59,151 +80,166 @@ export default function AddRecipe() {
 	}
 
 	return (
-		<Flex justifyContent='center' alignItems='center' flexDirection='column'>
-			<Heading as='h1' fontSize='7xl' textAlign='center'>
-				Add Recipe
-			</Heading>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault()
-					handleSubmit()
-					alert('Рецептік додано')
-				}}
-			>
-				<Grid
-					h='200px'
-					templateRows='repeat(2, 1fr)'
-					templateColumns={['repeat(1,1fr)', 'repeat(5, 1fr)']}
-					gap={2}
+		<>
+			<Flex justifyContent='center' alignItems='center' flexDirection='column'>
+				<Heading as='h1' fontSize='7xl' textAlign='center'>
+					Add Recipe
+				</Heading>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault()
+						handleSubmit()
+						alert('Рецептік додано')
+					}}
 				>
-					<GridItem colSpan={[1, 2]}>
-						<FormControl>
-							<FormLabel htmlFor='title'>Title</FormLabel>
-							<Input
-								name='title'
-								type='text'
-								placeholder='Awesome Crab Roll'
-								value={recipe.title}
-								onChange={handleChange}
-							/>
-						</FormControl>
-					</GridItem>
-					<GridItem colSpan={[1, 2]}>
-						<FormControl>
-							<FormLabel htmlFor='image'>Image URL</FormLabel>
-							<Input
-								name='image'
-								type='text'
-								placeholder='https://lorem.picsum'
-								value={recipe.image}
-								onChange={handleChange}
-							/>
-						</FormControl>
-					</GridItem>
-					<GridItem colSpan={[1, 2]}>
-						<FormControl>
-							<FormLabel htmlFor='description'>Description</FormLabel>
-							<AutoResizeTextarea
-								name='description'
-								minH={30}
-								placeholder='Tastes how I remember'
-								value={recipe.description}
-								onChange={handleChange}
-							/>
-						</FormControl>
-					</GridItem>
-					<GridItem colSpan={[1, 3]}>
-						<FormControl>
-							<FormLabel htmlFor='ingredients'>Ingredients</FormLabel>
-							<AutoResizeTextarea
-								minH={30}
-								name='ingredients'
-								value={recipe.ingredients}
-								onChange={handleChange}
-							/>
-						</FormControl>
-					</GridItem>
-					<GridItem colSpan={[1, 5]}>
-						<FormControl>
-							<FormLabel htmlFor='instructions'>Instructions</FormLabel>
-							<AutoResizeTextarea
-								name='instructions'
-								value={recipe.instructions}
-								onChange={handleChange}
-							/>
-						</FormControl>
-					</GridItem>
+					<Grid
+						h='200px'
+						templateRows='repeat(2, 1fr)'
+						templateColumns={['repeat(1,1fr)', 'repeat(5, 1fr)']}
+						gap={2}
+					>
+						<GridItem colSpan={[1, 2]}>
+							<FormControl>
+								<FormLabel htmlFor='title'>Title</FormLabel>
+								<Input
+									name='title'
+									type='text'
+									placeholder='Awesome Crab Roll'
+									value={recipe.title}
+									onChange={handleChange}
+								/>
+							</FormControl>
+						</GridItem>
+						<GridItem colSpan={[1, 2]}>
+							<FormControl>
+								<FormLabel htmlFor='image'>Image URL</FormLabel>
+								<Input
+									name='image'
+									type='text'
+									placeholder='https://lorem.picsum'
+									value={recipe.image}
+									onChange={handleChange}
+								/>
+							</FormControl>
+						</GridItem>
+						<GridItem colSpan={[1, 2]}>
+							<FormControl>
+								<FormLabel htmlFor='description'>Description</FormLabel>
+								<AutoResizeTextarea
+									name='description'
+									minH={30}
+									placeholder='Tastes how I remember'
+									value={recipe.description}
+									onChange={handleChange}
+								/>
+							</FormControl>
+						</GridItem>
+						<GridItem colSpan={[1, 3]}>
+							<FormControl>
+								<FormLabel htmlFor='ingredients'>Ingredients</FormLabel>
+								<AutoResizeTextarea
+									minH={30}
+									name='ingredients'
+									value={recipe.ingredients}
+									onChange={handleChange}
+								/>
+							</FormControl>
+						</GridItem>
+						<GridItem colSpan={[1, 5]}>
+							<FormControl>
+								<FormLabel htmlFor='instructions'>Instructions</FormLabel>
+								<AutoResizeTextarea
+									name='instructions'
+									value={recipe.instructions}
+									onChange={handleChange}
+								/>
+							</FormControl>
+						</GridItem>
 
-					<FormControl>
-						<FormLabel htmlFor='cookTime'>Cook Time</FormLabel>
-						<Input
-							name='cookTime'
-							type='text'
-							placeholder='in minutes'
-							value={recipe.cookTime}
-							onChange={handleChange}
-						/>
-					</FormControl>
-					<FormControl>
-						<FormLabel htmlFor='servings'>Servings</FormLabel>
-						<Input
-							name='servings'
-							type='number'
-							placeholder='5'
-							value={recipe.servings}
-							onChange={handleChange}
-						/>
-					</FormControl>
-					<FormControl>
-						<FormLabel htmlFor='calories'>Calories</FormLabel>
-						<Input
-							name='calories'
-							type='number'
-							placeholder='300'
-							value={recipe.calories}
-							onChange={handleChange}
-						/>
-					</FormControl>
-					<FormControl>
-						<FormLabel htmlFor='category'>Category</FormLabel>
-						<Select name='category' onChange={handleChange}>
-							{categories.map((category, index) => (
-								<option key={index} value={category}>
-									{category}
-								</option>
-							))}
-						</Select>
-					</FormControl>
-					<FormControl>
-						<FormLabel htmlFor='prepTime'>Prep Time</FormLabel>
-						<Input
-							name='prepTime'
-							type='text'
-							placeholder='Prep in minutes'
-							value={recipe.prepTime}
-							onChange={handleChange}
-						/>
-					</FormControl>
-					<GridItem colSpan={[1, 2]}>
-						<Button colorScheme='purple' width='100%' type='submit'>
-							Submit recipe
-						</Button>
-					</GridItem>
-					<GridItem colSpan={[1, 2]}>
-						<Link passHref href='/'>
-							<Button
-								colorScheme='purple'
-								variant='outline'
-								as='a'
-								width='100%'
-								type=''
-							>
-								Cancel
+						<FormControl>
+							<FormLabel htmlFor='cookTime'>Cook Time</FormLabel>
+							<Input
+								name='cookTime'
+								type='text'
+								placeholder='in minutes'
+								value={recipe.cookTime}
+								onChange={handleChange}
+							/>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='servings'>Servings</FormLabel>
+							<Input
+								name='servings'
+								type='number'
+								placeholder='5'
+								value={recipe.servings}
+								onChange={handleChange}
+							/>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='calories'>Calories</FormLabel>
+							<Input
+								name='calories'
+								type='number'
+								placeholder='300'
+								value={recipe.calories}
+								onChange={handleChange}
+							/>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='category'>Category</FormLabel>
+							<Select name='category' onChange={handleChange}>
+								{categories.map((category, index) => (
+									<option key={index} value={category}>
+										{category}
+									</option>
+								))}
+							</Select>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='prepTime'>Prep Time</FormLabel>
+							<Input
+								name='prepTime'
+								type='text'
+								placeholder='Prep in minutes'
+								value={recipe.prepTime}
+								onChange={handleChange}
+							/>
+						</FormControl>
+						<GridItem colSpan={[1, 5]}>
+							<FormControl>
+								<FormLabel htmlFor='imagetest'>Search Image</FormLabel>
+								<SearchIcon position='fixed top-0 left-0 right-0' />
+								<Input
+									name='findimage'
+									type='text'
+									placeholder='Search Image'
+									value={query}
+									onChange={(e) => setQuery(e.target.value)}
+								></Input>
+							</FormControl>
+						</GridItem>
+						<GridItem colSpan={[1, 2]}>
+							<Button colorScheme='purple' width='100%' type='submit'>
+								Submit recipe
 							</Button>
-						</Link>
-					</GridItem>
-				</Grid>
-			</form>
-		</Flex>
+						</GridItem>
+						<GridItem colSpan={[1, 2]}>
+							<Link passHref href='/'>
+								<Button
+									colorScheme='purple'
+									variant='outline'
+									as='a'
+									width='100%'
+									type=''
+								>
+									Cancel
+								</Button>
+							</Link>
+						</GridItem>
+					</Grid>
+				</form>
+			</Flex>
+		</>
 	)
 }
