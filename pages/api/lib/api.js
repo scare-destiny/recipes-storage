1097f630247b0a27f6a982d25b4494f9bc4e1be7
scare-destiny <zhenya.venger@gmail.com>
2022-10-06 21:43:47 +0300

    set no-cors property to fetch

--- pages/api/lib/api.js
+++ pages/api/lib/api.js
@@ -1,18 +1,14 @@
 const API_KEY = process.env.NEXT_PUBLIC_PEXELS_API_KEY
+import Cors from 'cors'
 
-export const getCuratedPhotos = async () => {
-	const res = await fetch(`https://api.pexels.com/v1/curated?per_page=8`, {
-		headers: {
-			Authorization: API_KEY,
-		},
-	})
-	const responseJson = await res.json()
-	return responseJson.photos
-}
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+})
 
 export const getQueryPhotos = async (query) => {
 	const res = await fetch(
 		`https://api.pexels.com/v1/search?query=${query}&per_page=8`,
+		{ mode: 'no-cors' },
 		{
 			headers: {
 				Authorization: API_KEY,
