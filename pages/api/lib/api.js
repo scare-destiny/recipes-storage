bd58e44d61068774ccec7b2d4a1672089508c7c9
scare-destiny <zhenya.venger@gmail.com>
2022-10-07 19:01:06 +0300

    hmm

--- pages/api/lib/api.js
+++ pages/api/lib/api.js
@@ -1,11 +1,20 @@
 const API_KEY = process.env.NEXT_PUBLIC_PEXELS_API_KEY
+import Cors from 'cors'
+
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+})
 
 export const getQueryPhotos = async (query) => {
-	const res = await fetch(`https://api.pexels.com/v1/search?query=${query}`, {
-		headers: {
-			Authorization: API_KEY,
-		},
-	})
+	const res = await fetch(
+		`https://api.pexels.com/v1/search?query=${query}&per_page=8`,
+		{ mode: 'no-cors' },
+		{
+			headers: {
+				Authorization: API_KEY,
+			},
+		}
+	)
 	const responseJson = await res.json()
 	return responseJson.photos
 }
