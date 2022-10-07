9069a9475807d319f857559d8eaabca05851765c
scare-destiny <zhenya.venger@gmail.com>
2022-10-07 20:11:11 +0300

    look at me

--- pages/addrecipe.js
+++ pages/addrecipe.js
@@ -81,8 +81,12 @@ export default function AddRecipe({ data }) {
 		// const auth = getAuth()
 		// await signInWithCustomToken(auth, firebaseClerkToken)
 <<<<<<< HEAD
+<<<<<<< HEAD
 		if (recipe.image !== '') {
 			const result = await addDoc(collection(database, 'recipes'), recipe)
+=======
+		const result = await addDoc(collection(database, 'recipes'), recipe)
+>>>>>>> parent of 8d002ee (added image search by image)
 =======
 		const result = await addDoc(collection(database, 'recipes'), recipe)
 >>>>>>> parent of 8d002ee (added image search by image)
