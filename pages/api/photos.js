c03f1e54a4fd6cdfac3b7d672c31b3e07aeb55fa
scare-destiny <zhenya.venger@gmail.com>
2022-10-13 20:01:41 +0300

    added env vars

--- pages/api/photos.js
+++ pages/api/photos.js
@@ -1,13 +1,8 @@
-const API_KEY = process.env.NEXT_PUBLIC_PEXELS_API_KEY
-
 export default async function handler(req, res) {
 	try {
 		const request = await fetch(`https://api.pexels.com/v1/search?query=test`, {
 			headers: {
-				'Authorization': API_KEY,
-				// update with your user-agent
-				'Accept': 'application/json, text/plain, */*',
-				'User-Agent': '*',
+				Authorization: process.env.NEXT_PUBLIC_PEXELS_API_KEY,
 			},
 		})
 		console.log('Entered the serverless function')
