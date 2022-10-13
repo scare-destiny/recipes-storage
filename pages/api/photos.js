0f151b0b8cfc55382ce57ecafba768d6288b37ec
scare-destiny <zhenya.venger@gmail.com>
2022-10-13 15:14:07 +0300

    added return to api route

--- pages/api/photos.js
+++ pages/api/photos.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 		})
 		console.log(req)
 		const data = await request.json()
-		res.status(200).json(data.photos)
+		return res.status(200).json(data.photos)
 	} catch (error) {
 		console.log(error)
 	}
