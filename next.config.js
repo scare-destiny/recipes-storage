286a0936209b418b2c0ed79bf93ce0e5a071ed74
scare-destiny <zhenya.venger@gmail.com>
2022-10-13 20:14:55 +0300

    hooray?

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
 		FACEBOOK_CLIENT_SECRET: 'ee0cf361036cdda8a3d764ffc1305cc1',
 	},
 	images: {
-		domains: ['images.pexels.com'],
+		domains: ['images.pexels.com', 'www.pexels.com'],
 	},
 	async headers() {
 		return [
