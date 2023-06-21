5ca73cab576e8a4eb75ab2f7e71960a555a2231f
scare-destiny <zhenya.venger@gmail.com>
2023-06-21 21:57:14 +0300

    hide keys

--- firebase.js
+++ firebase.js
@@ -1,16 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { initializeApp, getApps } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite'
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDLJtMYt6rlArHejqMlsQknDjPrJhpj_ww",
-  authDomain: "recipe-app-13554.firebaseapp.com",
-  databaseURL: "https://recipe-app-13554-default-rtdb.firebaseio.com",
-  projectId: "recipe-app-13554",
-  storageBucket: "recipe-app-13554.appspot.com",
-  messagingSenderId: "408634837228",
-  appId: "1:408634837228:web:9f045a0e00d51b0b727dc9"
-};
-const app = initializeApp(firebaseConfig);
-const database = getFirestore(app);
 
-export { database };
+console.log(process.env.NEXT_PUBLIC_authDomain)
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_authDomain,
+	databaseURL: process.env.databaseURL,
+	projectId: process.env.NEXT_PUBLIC_projectId,
+	storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+	messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+	appId: process.env.NEXT_PUBLIC_appId,
+}
+let app
+if (!getApps().length) {
+	app = initializeApp(firebaseConfig)
+} else {
+	app = getApps()[0] // if already initialized, use that one
+}
+
+const database = getFirestore(app)
+
+export { database }
