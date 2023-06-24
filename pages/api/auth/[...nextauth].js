34f9d5fb73153f8af94b04edfb7a541f2ae79bf8
scare-destiny <zhenya.venger@gmail.com>
2023-06-24 17:28:01 +0300

    google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,15 @@
 import NextAuth from 'next-auth'
 import FacebookProvider from 'next-auth/providers/facebook'
-
+import GoogleProvider from 'next-auth/providers/google'
 export const authOptions = {
 	providers: [
 		FacebookProvider({
 			clientId: process.env.FACEBOOK_CLIENT_ID,
 			clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
 	secret: 'teeest',
