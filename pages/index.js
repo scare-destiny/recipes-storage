f9231b49fa2feb5dda36311d5aef0eacbb4ba786
scare-destiny <zhenya.venger@gmail.com>
2023-06-29 20:13:11 +0300

    demo mode for unauth users

--- pages/index.js
+++ pages/index.js
@@ -10,39 +10,37 @@ import { collection, getDocs } from 'firebase/firestore/lite'
 import { database } from '../firebase'
 import { motion } from 'framer-motion'
 
-const MotionSimpleGrid = motion(SimpleGrid)
-const MotionRecipeCard = motion(RecipeCard)
-
 export default function Home() {
 	const { data: session } = useSession()
 
 	const [filter, setFilter] = useState('')
-	const [recipesFetched, setRecipesFetched] = useState(false)
+	// const [recipesFetched, setRecipesFetched] = useState(false)
 	const [recipes, setRecipes] = useState([])
 
+	async function getRecipes(email) {
+		const recipeCollection = collection(database, 'users', email, 'recipes')
+		const recipeSnapshot = await getDocs(recipeCollection)
+		const recipes = recipeSnapshot.docs.map((doc) => {
+			const data = doc.data()
+			data.id = doc.id
+			return data
+		})
+		console.log(`recipes are ${recipes}`)
+		setRecipes(recipes)
+
+		// setRecipesFetched(true)
+	}
+
 	useEffect(() => {
-		if (session && !recipesFetched) {
-			async function getRecipes() {
-				const recipeCollection = collection(
-					database,
-					'users',
-					session.user.email,
-					'recipes'
-				)
-				const recipeSnapshot = await getDocs(recipeCollection)
-				const recipes = recipeSnapshot.docs.map((doc) => {
-					const data = doc.data()
-					data.id = doc.id
-					return data
-				})
-				setRecipes(recipes)
-				setRecipesFetched(true)
-			}
-			getRecipes()
-		} else if (!session) {
-			setRecipesFetched(false)
+		if (session) {
+			getRecipes(session.user.email)
+			setRecipes(recipes)
+		} else {
+			// setRecipesFetched(false)
+			getRecipes('zhenya.venger@gmail.com')
+			setRecipes(recipes)
 		}
-	}, [session])
+	}, [])
 
 	useEffect(() => {
 		window.localStorage.setItem('filter', filter)
@@ -55,71 +53,35 @@ export default function Home() {
 	const recipesToShow = filter !== 'all' ? filteredRecipes : recipes
 
 	const handleFilterChange = ({ target }) => {
-		console.log(target.value)
-		console.log()
 		filter !== target.value ? setFilter(target.value) : setFilter('all')
 	}
-	const gridAnimationVariants = {
-		hidden: {
-			opacity: 0,
-		},
-		visible: {
-			opacity: 1,
-			transition: {
-				delay: 0.5,
-				when: 'beforeChildren',
-				staggerChildren: 0.1,
-			},
-		},
-	}
 
-	const recipeAnimationVariants = {
-		hidden: { opacity: 0, scale: 0.9 },
-		visible: {
-			opacity: 1,
-			scale: 1.5,
-			transition: {
-				duration: 0.3,
-			},
-		},
-	}
 	return (
 		<Flex justifyContent='center' alignItems='center' flexDirection='column'>
 			<Head>
 				<title>Recipe App</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			{session ? (
-				<>
-					<Filter categories={categories} handleFilterChange={handleFilterChange} />
+			<>
+				<Filter categories={categories} handleFilterChange={handleFilterChange} />
 
-					<MotionSimpleGrid
-						columns={[1, 2, 3]}
-						spacing={4}
-						initial='hidden'
-						animate='visible'
-						variants={gridAnimationVariants}
-					>
-						{recipesToShow.map((recipe, index) => (
-							<MotionRecipeCard
-								key={`${filter}-${index}`}
-								recipe={recipe}
-								initial='hidden'
-								animate='visible'
-								exit='hidden'
-								variants={recipeAnimationVariants}
-							/>
-						))}
-					</MotionSimpleGrid>
-				</>
-			) : (
-				<>
-					<Heading pt='4'>Login to add and edit yor recipes</Heading>
-					<Button mt='4' colorScheme='purple'>
-						<Link href='/addrecipe'>Add recipe</Link>
-					</Button>
-				</>
-			)}
+				<SimpleGrid
+					columns={[1, 2, 3]}
+					spacing={4}
+					initial='hidden'
+					animate='visible'
+				>
+					{recipesToShow.map((recipe, index) => (
+						<RecipeCard
+							key={`${filter}-${index}`}
+							recipe={recipe}
+							initial='hidden'
+							animate='visible'
+							exit='hidden'
+						/>
+					))}
+				</SimpleGrid>
+			</>
 		</Flex>
 	)
 }
